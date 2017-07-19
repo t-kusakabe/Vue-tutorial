58248a2279fe38d48ccc6413515be5552c2ec075
t-kusakabe <ttmac0704@gmail.com>
2017-07-20 02:53:29 +0900

    create reversedMessage function

--- src/main.js
+++ src/main.js
@@ -18,3 +18,20 @@ new Vue({
     message: 'test'
   }
 })
+
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    reversedMessage: function() {
+      return this.message.split('').reverse().join('')
+    }
+  },
+  methods: {
+    reversedMessage: function() {
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
